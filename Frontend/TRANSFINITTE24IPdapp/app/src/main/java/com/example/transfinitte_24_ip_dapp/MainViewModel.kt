943d8c3a55ff79f0724490ab7cbec5bab2a98ac4
Allen-Sumanth <allensumanth.07@gmail.com>
2024-10-19 14:40:46 +0530

    Add sample function

--- Frontend/TRANSFINITTE24IPdapp/app/src/main/java/com/example/transfinitte_24_ip_dapp/MainViewModel.kt
+++ Frontend/TRANSFINITTE24IPdapp/app/src/main/java/com/example/transfinitte_24_ip_dapp/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.transfinitte_24_ip_dapp
 
+import android.util.Log
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.web3j.protocol.Web3j
 import javax.inject.Inject
@@ -8,5 +9,13 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val web3j: Web3j
 ) {
+    fun getWalletAddress() {
+        web3j.ethAccounts().flowable()
+            .subscribe({ result ->
+                Log.d("Wallet Address", result.accounts[0])
+            }, { error ->
+                Log.e("Error", error.message ?: "Unknown error")
+            })
+    }
 
 }
