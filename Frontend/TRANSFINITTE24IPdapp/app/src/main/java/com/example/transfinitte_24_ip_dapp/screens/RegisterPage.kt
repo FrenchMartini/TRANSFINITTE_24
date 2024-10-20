cdac936aee1545ddd3db8ddab5dbca595b4ae478
Allen-Sumanth <allensumanth.07@gmail.com>
2024-10-20 12:03:16 +0530

    all features except search done

--- Frontend/TRANSFINITTE24IPdapp/app/src/main/java/com/example/transfinitte_24_ip_dapp/screens/RegisterPage.kt
+++ Frontend/TRANSFINITTE24IPdapp/app/src/main/java/com/example/transfinitte_24_ip_dapp/screens/RegisterPage.kt
@@ -1,5 +1,8 @@
 package com.example.transfinitte_24_ip_dapp.screens
 
+import CONTRACT_ADDRESS
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -12,7 +15,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -24,23 +29,40 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import com.example.transfinitte_24_ip_dapp.MainViewModel
+import com.example.transfinitte_24_ip_dapp.backend.RegistrationBase
 import com.example.transfinitte_24_ip_dapp.navigation.Screens
 import com.example.transfinitte_24_ip_dapp.ui.theme.alumniSans
 import com.example.transfinitte_24_ip_dapp.ui.theme.anticRegular
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 @Composable
-fun RegisterPage(navController: NavController) {
+fun RegisterPage(navController: NavController, viewModel: MainViewModel, context: Context) {
+    val localCoroutineScope = rememberCoroutineScope()
+    var titleField by remember {
+        mutableStateOf("")
+    }
+    var appNumber by remember {
+        mutableStateOf("")
+    }
+    var abstractField by remember { mutableStateOf("") }
+    var description by remember {
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -55,64 +77,161 @@ fun RegisterPage(navController: NavController) {
             fontFamily = alumniSans
         )
         Spacer(modifier = Modifier.fillMaxSize(0.05f))
-        Card(
-            modifier = Modifier
-                .fillMaxWidth(0.9f)
-                .aspectRatio(.7f),
-            colors = CardDefaults.cardColors(
-                containerColor = Color.Black
-            ),
-            border = BorderStroke(1.dp, Color.Yellow),
-        ) {
-            Spacer(modifier = Modifier.height(20.dp))
-
-            Row (
+        Column {
+            Card(
                 modifier = Modifier
-                    .align(Alignment.CenterHorizontally),
-                verticalAlignment = Alignment.CenterVertically
+                    .fillMaxWidth(0.9f)
+                    .aspectRatio(.6f),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.Black
+                ),
+                border = BorderStroke(1.dp, Color.Yellow),
             ) {
-                var titleField by remember {
-                    mutableStateOf("")
-                }
-                TextField(
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth(0.9f)
-                        .height(70.dp),
-                    value = "Title",
-                    onValueChange = {
-                        titleField = it
-                    },
-                    singleLine = true,
-                    colors = TextFieldDefaults.colors(
-                        unfocusedContainerColor = Color.Transparent,
-                        focusedContainerColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.White,
-                        focusedIndicatorColor = Color.White,
-                        unfocusedTextColor = Color.LightGray,
-                        focusedTextColor = Color.LightGray,
-                        focusedSupportingTextColor = Color.LightGray
-                    ),
-                    placeholder = {
-                        Text(
-                            text = "Title",
-                            color = Color.Gray,
-                            fontSize = 30.sp,
-                            textAlign = TextAlign.Center,
+                        .align(Alignment.CenterHorizontally)
+                        .verticalScroll(rememberScrollState()),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(modifier = Modifier.height(20.dp))
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth(0.9f)
+                            .height(60.dp),
+                        value = titleField,
+                        onValueChange = {
+                            titleField = it
+                        },
+                        singleLine = true,
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = Color.Transparent,
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.White,
+                            focusedIndicatorColor = Color.White,
+                            unfocusedTextColor = Color.LightGray,
+                            focusedTextColor = Color.LightGray,
+                            focusedSupportingTextColor = Color.LightGray
+                        ),
+                        placeholder = {
+                            Text(
+                                text = "Title",
+                                color = Color.Gray,
+                                fontSize = 25.sp,
+                                textAlign = TextAlign.Center,
+                            )
+                        },
+                        maxLines = 1,
+                        textStyle = TextStyle.Default.copy(fontSize = 30.sp),
+                    )
+
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth(0.9f)
+                            .height(60.dp),
+                        value = appNumber,
+                        onValueChange = {
+                            appNumber = it
+                        },
+                        singleLine = true,
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = Color.Transparent,
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.White,
+                            focusedIndicatorColor = Color.White,
+                            unfocusedTextColor = Color.LightGray,
+                            focusedTextColor = Color.LightGray,
+                            focusedSupportingTextColor = Color.LightGray
+                        ),
+                        placeholder = {
+                            Text(
+                                text = "Application Number",
+                                color = Color.Gray,
+                                fontSize = 25.sp,
+                                textAlign = TextAlign.Center,
+                            )
+                        },
+                        maxLines = 1,
+                        textStyle = TextStyle.Default.copy(fontSize = 30.sp)
+                    )
+
+                    val charLimit = 1500
+                    val focusManager = LocalFocusManager.current
+
+                    //region abstract
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Text(
+                        "Abstract",
+                        color = Color.Gray,
+                        modifier = Modifier
+                            .align(Alignment.Start)
+                            .padding(start = 10.dp),
+                        fontSize = 25.sp
+                    )
+                    TextField(
+                        value = abstractField,
+                        onValueChange = {
+                            abstractField = it.take(charLimit)
+                            if (it.length > charLimit) {
+                                focusManager.moveFocus(FocusDirection.Down)
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth(0.9f)
+                            .padding(top = 10.dp),
+                        minLines = 20,
+                        maxLines = 20,
+                        shape = RoundedCornerShape(15.dp),
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = Color.LightGray.copy(alpha = 0.28f),
+                            focusedContainerColor = Color.LightGray.copy(alpha = 0.28f),
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent,
+                            focusedTextColor = Color.White,
+                            unfocusedTextColor = Color.White
                         )
-                    },
-                    maxLines = 1,
-                    textStyle = TextStyle.Default.copy(fontSize = 30.sp),
-                )
+                    )
+                    //endregion
+
+                    //region description
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Text(
+                        "Description",
+                        color = Color.Gray,
+                        modifier = Modifier
+                            .align(Alignment.Start)
+                            .padding(start = 10.dp),
+                        fontSize = 25.sp
+                    )
+                    TextField(
+                        value = description,
+                        onValueChange = {
+                            description = it.take(charLimit)
+                            if (it.length > charLimit) {
+                                focusManager.moveFocus(FocusDirection.Down)
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth(0.9f)
+                            .padding(top = 10.dp),
+                        minLines = 20,
+                        maxLines = 20,
+                        shape = RoundedCornerShape(15.dp),
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = Color.LightGray.copy(alpha = 0.28f),
+                            focusedContainerColor = Color.LightGray.copy(alpha = 0.28f),
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent,
+                            focusedTextColor = Color.White,
+                            unfocusedTextColor = Color.White
+                        )
+                    )
+                    //endregion
+
+                    Spacer(modifier = Modifier.height(20.dp))
+                }
             }
-
-
-            Spacer(modifier = Modifier.height(10.dp))
-            Text(
-                "Abstract",
-                color = Color.White,
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                fontSize = 20.sp
-            )
         }
 
         Row(
@@ -123,7 +242,26 @@ fun RegisterPage(navController: NavController) {
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             Button(
-                onClick = {},
+                onClick = {
+                    localCoroutineScope.launch {
+                        val isRegistered = localCoroutineScope.async {
+                            viewModel.registerPatent(
+                                RegistrationBase(
+                                    title = titleField,
+                                    description = description,
+                                    userAddress = CONTRACT_ADDRESS
+                                )
+                            )
+                        }
+
+                        if (isRegistered.await()){
+                            Toast.makeText(context, "Patent Registered!", Toast.LENGTH_SHORT).show()
+                            navController.navigate(Screens.LandingPage.route)
+                        } else {
+                            Toast.makeText(context, "Registration unsuccessful", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                },
                 modifier = Modifier.width(120.dp),
                 shape = RoundedCornerShape(10.dp),
                 colors = ButtonDefaults.buttonColors(
@@ -140,7 +278,7 @@ fun RegisterPage(navController: NavController) {
             }
             Button(
                 onClick = {
-                    navController.navigate(Screens.RegisterPage.route)
+                    navController.navigate(Screens.LandingPage.route)
                 },
                 modifier = Modifier.width(120.dp),
                 shape = RoundedCornerShape(10.dp),
@@ -158,10 +296,4 @@ fun RegisterPage(navController: NavController) {
             }
         }
     }
-}
-
-@Preview
-@Composable
-private fun RegisterPrev() {
-    RegisterPage(rememberNavController())
 }
